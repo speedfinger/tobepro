3bd86ebd3bc1e6afcfb3d1c05c320bf90ddc9b15
SUNGLAEKIM <laenuss@gmail.com>
2024-07-28 18:52:49 +0900

    lae19/20

--- dfsbfs/lae_20.py
+++ dfsbfs/lae_20.py
@@ -1,59 +1,61 @@
 import sys
 from collections import deque
 
-if sys.platform == 'win32':
-    sys.stdin = open('lae_20.txt')
+if sys.platform == "win32":
+    sys.stdin = open("input.txt")
 
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-SR, SC, ER, EC = map(int, input().split())
+R, C = map(int, input().split())
+KR, KC, PR, PC = map(int, input().split())
 
-visited = [[0] * (M + 1) for _ in range(N + 1)]
+visited = [[0]*(C+1) for _ in range(R+1)]
 
 dr = [-2, -1, 1, 2, 2, 1, -1, -2]
 dc = [1, 2, 2, 1, -1, -2, -2, -1]
 
-INF = 987654321
+visit = -1
 
-def bfs1():
-    que = deque([(SR, SC, 0)])
-    visited[SR][SC] = 1
+def bfsQue():
+    que = deque([(KR, KC, 0)])
+    visited[KR][KC] = 1
 
-    while que:
+    while(que):
         r, c, dist = que.popleft()
-
-        if (r, c) == (ER, EC): return dist
-
+        if r == PR and c == PC:
+            return dist
         for i in range(8):
-            nr, nc = r + dr[i], c + dc[i]
-            if nr < 1 or nr > N or nc < 1 or nc > M: continue
-            if visited[nr][nc]: continue
-            que.append((nr, nc, dist + 1))
-            visited[nr][nc] = 1
-
+            nr = r+dr[i]
+            nc = c+dc[i]
+            if nr < 1 or nr > R or nc < 1 or nc > C:
+                continue
+            else:
+                if visited[nr][nc] == 1:
+                    continue
+                else:
+                    que.append((nr,nc,dist+1))
+                    visited[nr][nc] = 1
     return -1
 
+# print(bfsQue())
 
-def bfs2():
-    que = deque([(SR, SC)])
-    visited[SR][SC] = 0
+def bfsMat():
+    que = deque([(KR, KC)])
+    visited[KR][KC] = 0
 
-    while que:
+    while(que):
         r, c = que.popleft()
-
-        if (r, c) == (ER, EC): return visited[r][c]
-
+        if r == PR and c == PC:
+            return visited[r][c]
         for i in range(8):
-            nr, nc = r + dr[i], c + dc[i]
-            if nr < 1 or nr > N or nc < 1 or nc > M: continue
-            if visited[nr][nc] <= visited[r][c] + 1: continue
-            que.append((nr, nc))
-            visited[nr][nc] = visited[r][c] + 1
-
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if nr < 1 or nr > R or nc < 1 or nc > C:
+                continue
+            if visited[nr][nc] == -1:
+                que.append((nr,nc))
+                visited[nr][nc] = visited[r][c]+1
     return -1
 
-# print(bfs1())
-
-visited = [[INF] * (M + 1) for _ in range(N + 1)]
-print(bfs2())
+visited = [[visit]*(C+1) for _ in range(R+1)]
+print(bfsMat())
