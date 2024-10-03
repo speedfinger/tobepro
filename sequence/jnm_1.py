1d99094e4e7bdaca9f7e9247610dd9461dbdd69b
z2m <speedfinger@naver.com>
2024-10-03 20:18:45 +0900

    init sequence

---
+++ sequence/jnm_1.py
@@ -0,0 +1,11 @@
+"""
+색종이(초)
+https://codepass.co.kr/contest/406/problem/1?cursor=eyJwcm9ibGVtc2V0IjoiY180MDYiLCJmaWVsZCI6MCwiaWR4IjowfQ==
+
+
+"""
+
+import sys
+
+
+sys.stdin = open("./sequence/jnm_1.txt",'r')
